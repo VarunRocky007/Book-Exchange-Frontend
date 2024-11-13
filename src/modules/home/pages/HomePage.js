6052e84426efbdb8c9b5e3730192950b53dfe895
varuns <varsho2002@gmail.com>
2024-11-13 08:16:14 +0530

    Remove Pagination when no books available

--- src/modules/home/pages/HomePage.js
+++ src/modules/home/pages/HomePage.js
@@ -1,71 +1,72 @@
 import "./HomePage.css";
 import Header from "../../../components/header/Header";
 import useHandleListOfItem from "../hooks/useHandleListOfItems";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 const HomePage = () => {
 
-  const {
-    loading,
-    error,
-    items,
-    fetchItems,
-    currentPage,
-    totalPages,
-    setCurrentPage
-  } = useHandleListOfItem();
+    const {
+        loading,
+        error,
+        items,
+        fetchItems,
+        currentPage,
+        totalPages,
+        setCurrentPage
+    } = useHandleListOfItem();
 
-  const handleNextPage = async () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage+1);
-    }
-    await fetchItems(currentPage+1);
-  };
+    const handleNextPage = async () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+        await fetchItems(currentPage + 1);
+    };
 
-  const handlePreviousPage = async () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage-1);
-    }
-    await fetchItems(currentPage-1);
-  };
+    const handlePreviousPage = async () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+        await fetchItems(currentPage - 1);
+    };
 
 
-  useEffect(() => {
-    async function fetchData() {
-      await fetchItems();
-    }
-    fetchData();
-  }, []);
-  return (
-    <div className="homePage">
-      <Header />
-      {loading && (<span>...Loading</span>)}
-      {error !== "" && (<span>{error}</span>)}
-      <div className="container">
-        <div className="card-container">
-          {items.map((item) => (
-            <div key={item.id} className="card">
-              <h2 className="card-title">{item.title}</h2>
-              <p className="card-author">Author: {item.author}</p>
-              <p className={"card-availability"}>
-                Status: {item.availabilityStatus}
-              </p>
-              <p className="card-genre">Genre: {item.genre}</p>
+    useEffect(() => {
+        async function fetchData() {
+            await fetchItems();
+        }
+
+        fetchData();
+    }, []);
+    return (
+        <div className="homePage">
+            <Header/>
+            {loading && (<span>...Loading</span>)}
+            {error !== "" && (<span>{error}</span>)}
+            <div className="container">
+                <div className="card-container">
+                    {items.map((item) => (
+                        <div key={item.id} className="card">
+                            <h2 className="card-title">{item.title}</h2>
+                            <p className="card-author">Author: {item.author}</p>
+                            <p className={"card-availability"}>
+                                Status: {item.availabilityStatus}
+                            </p>
+                            <p className="card-genre">Genre: {item.genre}</p>
+                        </div>
+                    ))}
+                </div>
+                {totalPages !== 0 && (<div className="pagination">
+                    <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                        Previous
+                    </button>
+                    <span>Page {currentPage} of {totalPages}</span>
+                    <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                        Next
+                    </button>
+                </div>)}
             </div>
-          ))}
         </div>
-        <div className="pagination">
-          <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-            Previous
-          </button>
-          <span>Page {currentPage} of {totalPages}</span>
-          <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-            Next
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
